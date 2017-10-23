7c84affb61a72994515e55986bcfe21de1194be7
Thomas Moser <mail@thomasmoser.ch>
2017-10-23 15:37:57 +0200

    feat: Added support for storybook Knobs

--- stories/index.js
+++ stories/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, text } from '@storybook/addon-knobs';
 import FullWithImage from '../src/components/full-with-image'
 
+
 storiesOf('FullWithImage', module)
-  .add('full with image', () =>(
-    <FullWithImage assetType=".jpg" assetName="thomas_moser_3" altText="This is Thomas"/>
+  .addDecorator(withKnobs)
+  .add('full with image', () => (
+    <FullWithImage
+      assetType={text('Asset Type','.jpg')}
+      assetName={text('Asset Name','thomas_moser_3')}
+      altText={text('Alternative Text','This is Thomas')}
+    />
   ))
