71ece171ee8cca68eeace1fac2735e9c18abcc5b
Thomas Moser <mail@thomasmoser.ch>
2017-10-23 16:14:31 +0200

    feat: Added Helmet for changing the Head of my react app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import CurriculumVitae from './components/curriculum-vitae'
+import { Helmet } from "react-helmet";
 
 class App extends Component {
   render() {
     return (
-      <CurriculumVitae />
+      <div>
+        <Helmet>
+          <title>This is my Title</title>
+        </Helmet>
+        <CurriculumVitae />
+      </div>
     );
   }
 }
