8fb68dc7568f96565af8f333e109414592348fc8
Thomas Moser <mail@thomasmoser.ch>
2017-10-24 11:18:18 +0200

    feat: Added Todos and basic sections for all parts of the project

--- src/App.js
+++ src/App.js
@@ -3,21 +3,50 @@ import CurriculumVitae from './components/curriculum-vitae'
 import { Helmet } from "react-helmet";
 import styled from 'styled-components'
 
+//TODO: Add React Router and add a 404 page
+//TODO: Maybe add Redux, although this could be a bit over engineered
+
 class App extends Component {
   render() {
     return (
       <div>
         <Helmet>
-          <title>This is my Title</title>
+          <title>Bewerbung Thomas Moser</title>
         </Helmet>
         <Header>
-          Header Content
+          {
+            //TODO: Title and maybe a Navigation (Mainly Text, maybe with some interaction)
+          }
+          <h1>CV Thomas Moser</h1>
         </Header>
         <Section>
           <CurriculumVitae />
         </Section>
+        <Section>
+          {
+            //TODO: Who am I (Mainly Text, maybe with some interaction)
+          }
+        </Section>
+        <Section>
+          {
+            //TODO: Why do I want this Job (Mainly Text, maybe with some interaction)
+          }
+        </Section>
+        <Section>
+          {
+            //TODO: Why am I suited for this Job (Mainly Text, maybe with some interaction)
+          }
+        </Section>
+        <Section>
+          {
+            //TODO: What I did so far
+            //This will be more complex and will contain a timeline, all certificates, and employer references including sample projects and prices won
+          }
+        </Section>
         <Footer>
-          Footer content
+          {
+            //TODO: Add Footer with adress and contact details
+          }
         </Footer>
       </div>
     );
