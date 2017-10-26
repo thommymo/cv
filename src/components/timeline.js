511c7f25f7cb009dfdc2097a5533d3e1e3d8d45f
Thomas Moser <mail@thomasmoser.ch>
2017-10-26 09:32:41 +0200

    refactor: Moved all timeline relevant features into timeline.js file

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import { primaryDark, white, primary, secondary, secondaryDark } from '../utils/colors'
 import { media } from '../utils/breakpoints'
-import { StyledP, StyledH3, TimeSpan} from '../components/styled-atoms'
+import { StyledP, StyledH3, StyledH4 } from '../components/styled-atoms'
 
 //TODO: Move Icon into CSS (Now it is loaded x times which doesn't make sense)
 
@@ -137,5 +137,78 @@ const StyledTitle = StyledH3.extend`
   ${media.tablet`margin-top: 31px;`}
   ${media.phone`margin-top: 34px;`}
 `
+const TimeSpan = ({
+  rightposition = false,
+  show = false,
+  startDate = false,
+  endDate = false,
+  right=false,
+  left=false,
+  bothleft=false,
+  bothright=false,
+}) => (
+  <Year borderright={bothright} borderleft={bothleft} rightposition={rightposition}>
+    <StyledTimeSpan show={show}>
+      { endDate &&
+        <span>{endDate}</span>
+      }
+      { startDate && endDate &&
+        <span>&#8599;</span>
+      }
+      { startDate &&
+        <span>{startDate}</span>
+      }
+    </StyledTimeSpan>
+    { left && show &&
+      <Bulletleft
+        src="data:image/svg+xml;charset=utf-8,%3Csvg%20fill%3D%22%23FFFFFF%22%20height%3D%2224%22%20viewBox%3D%220%200%2024%2024%22%20width%3D%2224%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%0D%0A%20%20%20%20%3Ccircle%20cx%3D%2212%22%20cy%3D%2212%22%20r%3D%2210%22%2F%3E%0D%0A%20%20%20%20%3Cpath%20d%3D%22M0%200h24v24H0z%22%20fill%3D%22none%22%2F%3E%0D%0A%3C%2Fsvg%3E"
+      />
+    }
+    { right && show &&
+      <Bulletright
+        src="data:image/svg+xml;charset=utf-8,%3Csvg%20fill%3D%22%23FFFFFF%22%20height%3D%2224%22%20viewBox%3D%220%200%2024%2024%22%20width%3D%2224%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%0D%0A%20%20%20%20%3Ccircle%20cx%3D%2212%22%20cy%3D%2212%22%20r%3D%2210%22%2F%3E%0D%0A%20%20%20%20%3Cpath%20d%3D%22M0%200h24v24H0z%22%20fill%3D%22none%22%2F%3E%0D%0A%3C%2Fsvg%3E"
+      />
+    }
+  </Year>
+);
 
+const Bulletright = styled.img`
+  ${media.desktop`right:61px;`};
+  ${media.tablet`right:61px;`};
+  ${media.phone`right:17px;`};
+  position: absolute;
+  top:75px;
+`
+const Bulletleft = styled.img`
+  ${media.desktop`left:61px;`};
+  ${media.tablet`left:61px;`};
+  ${media.phone`left:16px;`};
+  position: absolute;
+  top:75px;
+`
+const StyledTimeSpan = StyledH4.extend`
+  height:121px;
+  display:flex;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  ${media.desktop`line-height: 1em;`}
+  ${media.tablet`line-height: 1em;`}
+  ${media.phone`display: none;`}
+  ${props => !props.show ? 'display:none' : ''};
+`
+const Year = styled.div`
+  position: relative;
+  ${props => props.borderleft ? 'box-shadow: 6px 0 0 0 #FFF; ' : ''};
+  ${props => props.borderright ? 'box-shadow: -6px 0 0 0 #FFF; ' : ''};
+  ${media.desktop`width:50px;`};
+  ${media.desktop`min-width:50px;`};
+  ${media.tablet`width:50px;`};
+  ${media.tablet`min-width:50px;`};
+  ${media.phone`width:5px;`};
+  ${media.phone`min-width:5px;`};
+  color: ${white};
+  padding: 0 10px 0 10px;
+  z-index: 20;
+`
 export default Timeline
