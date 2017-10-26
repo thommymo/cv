40a4ca61f8aa1e0e0e1386c27d7151afade8c0cf
Thomas Moser <mail@thomasmoser.ch>
2017-10-26 08:40:02 +0200

    feat: Removed data from mobile view of timeline

--- src/components/styled-atoms.js
+++ src/components/styled-atoms.js
@@ -67,20 +67,32 @@ export const TimeSpan = ({
         <span>{startDate}</span>
       }
     </StyledTimeSpan>
-    <Bullet
-      show={show}
-      right={right}
-      left={left}
-      src="data:image/svg+xml;charset=utf-8,%3Csvg%20fill%3D%22%23FFFFFF%22%20height%3D%2224%22%20viewBox%3D%220%200%2024%2024%22%20width%3D%2224%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%0D%0A%20%20%20%20%3Ccircle%20cx%3D%2212%22%20cy%3D%2212%22%20r%3D%2210%22%2F%3E%0D%0A%20%20%20%20%3Cpath%20d%3D%22M0%200h24v24H0z%22%20fill%3D%22none%22%2F%3E%0D%0A%3C%2Fsvg%3E"
-    />
+    { left && show &&
+      <Bulletleft
+        src="data:image/svg+xml;charset=utf-8,%3Csvg%20fill%3D%22%23FFFFFF%22%20height%3D%2224%22%20viewBox%3D%220%200%2024%2024%22%20width%3D%2224%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%0D%0A%20%20%20%20%3Ccircle%20cx%3D%2212%22%20cy%3D%2212%22%20r%3D%2210%22%2F%3E%0D%0A%20%20%20%20%3Cpath%20d%3D%22M0%200h24v24H0z%22%20fill%3D%22none%22%2F%3E%0D%0A%3C%2Fsvg%3E"
+      />
+    }
+    { right && show &&
+      <Bulletright
+        src="data:image/svg+xml;charset=utf-8,%3Csvg%20fill%3D%22%23FFFFFF%22%20height%3D%2224%22%20viewBox%3D%220%200%2024%2024%22%20width%3D%2224%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%0D%0A%20%20%20%20%3Ccircle%20cx%3D%2212%22%20cy%3D%2212%22%20r%3D%2210%22%2F%3E%0D%0A%20%20%20%20%3Cpath%20d%3D%22M0%200h24v24H0z%22%20fill%3D%22none%22%2F%3E%0D%0A%3C%2Fsvg%3E"
+      />
+    }
   </Year>
 );
 
-const Bullet = styled.img`
-  ${props => props.left ? 'left:61px;':'right:61px'};
+const Bulletright = styled.img`
+  ${media.desktop`right:61px;`}
+  ${media.tablet`right:61px;`}
+  ${media.phone`right:17px;`}
   position: absolute;
   top:75px;
-  ${props => !props.show ? 'display:none' : ''};
+`
+const Bulletleft = styled.img`
+  ${media.desktop`left:61px;`}
+  ${media.tablet`left:61px;`}
+  ${media.phone`left:16px;`}
+  position: absolute;
+  top:75px;
 `
 const StyledTimeSpan = StyledH4.extend`
   height:121px;
@@ -90,7 +102,7 @@ const StyledTimeSpan = StyledH4.extend`
   text-align: center;
   ${media.desktop`line-height: 1em;`}
   ${media.tablet`line-height: 1em;`}
-  ${media.phone`line-height: 1em;`}
+  ${media.phone`display: none;`}
   ${props => !props.show ? 'display:none' : ''};
 `
 const Year = styled.div`
@@ -101,8 +113,8 @@ const Year = styled.div`
   ${media.desktop`min-width:50px;`};
   ${media.tablet`width:50px;`};
   ${media.tablet`min-width:50px;`};
-  ${media.phone`width:50px;`};
-  ${media.phone`min-width:50px;`};
+  ${media.phone`width:5px;`};
+  ${media.phone`min-width:5px;`};
   color: ${white};
   ${props => props.rightposition ? `background-color:${primaryDark};` : `background-color:${primary};`};
 
