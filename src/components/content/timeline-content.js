4e12c12ae24e3009e46c05726037897d2657aaec
Thomas Moser <mail@thomasmoser.ch>
2017-10-27 07:09:27 +0200

    refactor: Removed all static content from timeline

--- src/components/content/timeline-content.js
+++ src/components/content/timeline-content.js
@@ -5,10 +5,10 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 
-// here we create a query operation
+// here we create a grapphql operation
 const getEntriesQuery = gql`
   query getEntriesQuery {
-    allCVEntrieses(orderBy: startDate_DESC) {
+    allCVEntrieses(orderBy: order_ASC) {
       id
       showmore
       startDate
@@ -64,129 +64,6 @@ class TimelineContent extends Component {
             />
           ))
         }
-
-        <Timeline
-          bothleft
-          right
-          addBottomBorder
-          startDate="2015"
-          endDate="2017"
-          title="Fullstack Web Developer"
-          school="Udacity"
-          learned="Learning React, Node.js"
-          showmore
-        />
-        <Timeline
-          addTopBorder
-          left
-          bothleft
-          startDate="2015"
-          endDate="2017"
-          title="Projektleiter Web"
-          company="Luzerner Kantonsspital"
-          responsability="Responsible for Relaunching luks.ch"
-          showmore
-        />
-        <Timeline
-          left
-          bothleft
-          startDate="2011"
-          endDate="2015"
-          title="Product Owner"
-          company="Schweizer Radio und Fernsehen"
-          responsability="Responsible as Product Owner for srf.ch"
-          showmore
-        />
-        <Timeline
-          left
-          addBottomBorder
-          bothleft
-          startDate="2010"
-          endDate="2011"
-          title="Projektleiter Web"
-          company="Unic AG"
-          responsability="Responsible for customor web projects"
-          showmore
-        />
-        <Timeline
-          right
-          addTopBorder
-          bothleft
-          startDate="2003"
-          endDate="2010"
-          title="Master of Science UZH"
-          school="Universität Zürich"
-          learned="Media Science, Cognitive Psychology, Informatics"
-          showmore
-        />
-        <Timeline
-          left
-          bothright
-          startDate="2007"
-          endDate="2010"
-          title="Web Developer und Wissenschaftlicher Mitarbeiter"
-          school="Pädagogische Hochschule Zentralschweiz"
-          responsability="Responsible for customor web projects"
-          showmore
-        />
-        <Timeline
-          left
-          bothright
-          startDate="2006"
-          endDate="2008"
-          title="Web Developer und Mitinhaber"
-          company="Agentur Sign, Luzern"
-          responsability="Responsible for customor web projects"
-          showmore
-        />
-        <Timeline
-          left
-          bothright
-          startDate="2002"
-          endDate="2006"
-          title="Web Developer, selbständig erwerbend"
-          company="Netzwerk Neuweg, Luzern"
-          responsability="Responsible for customor web projects"
-          showmore
-        />
-        <Timeline
-          right
-          bothleft
-          startDate="2002"
-          endDate="2003"
-          title="Multimedia Producer SAE"
-          school="School of Audio Engenieering (SAE) Zürich"
-          learned="Multimedia"
-        />
-        <Timeline
-          left
-          bothright
-          addBottomBorder
-          startDate="2000"
-          endDate="2000"
-          title="Fahrradkurier"
-          company="Velokurier, Luzern"
-          responsability="Responsible for customor web projects"
-          showmore
-        />
-        <Timeline
-          right
-          addTopBorder
-          bothright
-          startDate="1995"
-          endDate="2002"
-          title="Matura Typus C"
-          school="Kantonsschule Rothen (LU)"
-          learned="Mainly Mathematics and Science"
-        />
-        <Timeline
-          right
-          startDate="1988"
-          endDate="1995"
-          title="Kindergarten und Primarschule"
-          school="Primarschule Luzern"
-        />
-
       </SectionNoOverflow>
     )
   }
