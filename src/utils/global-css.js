5a81fdc21477cc89ab118f12085f74fb6c84c723
Thomas Moser <mail@thomasmoser.ch>
2017-11-10 16:55:43 +0100

    refactor: Moved global css to it's own file

---
+++ src/utils/global-css.js
@@ -0,0 +1,62 @@
+import { injectGlobal } from 'styled-components';
+import { white } from './colors'
+import { timeout } from './constants'
+
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:600,400,200');
+
+  body:before {
+    height: 100vh;
+    display:block;
+  }
+  html, body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Source Sans Pro', sans-serif;
+    font-weight: 200;
+    font-size: 100%;
+    height: 100%;
+    background-color: ${white};
+  }
+  .FadeIn-appear {
+    opacity: 0;
+  }
+  .FadeIn-appear.FadeIn-appear-active {
+    opacity: 1;
+    transition: opacity ${(timeout*2)}ms ease-out ${(timeout)}ms;
+  }
+  .FadeIn-enter {
+    opacity: 0;
+  }
+  .FadeIn-enter.FadeIn-enter-active {
+    opacity: 1;
+    transition: opacity ${(timeout*2)}ms ease-out ${(timeout)}ms;
+  }
+  .SlideIn-appear {
+    opacity: 0;
+    transform: translateY(10px);
+  }
+  .SlideIn-appear.SlideIn-appear-active {
+    opacity: 1;
+    transform: translateY(0px);
+    transition: all ${(timeout*2)}ms ease-out ${(timeout*1.5)}ms;
+  }
+  .SlideIn-enter {
+    opacity: 0;
+    transform: translateY(10px);
+  }
+  .SlideIn-enter.SlideIn-enter-active {
+    opacity: 1;
+    transform: translateY(0px);
+    transition: all ${(timeout*2)}ms ease-out ${(timeout*1.5)}ms;
+  }
+  .SlideIn-exit {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+  .SlideIn-exit.SlideIn-exit-active {
+    opacity: 0;
+    transform: translateY(20px);
+    transition: all ${(timeout)}ms ease-out;
+  }
+`
