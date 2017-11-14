69ee5d1f9443245652b1acf5a5fdce976397c739
Thomas Moser <mail@thomasmoser.ch>
2017-11-14 09:29:51 +0100

    feat: Added content on abaoutme page.

--- src/components/pages/aboutme.js
+++ src/components/pages/aboutme.js
@@ -1,6 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 import PageShell from '../../components/templates/page-shell'
+import { H1, H2, P } from '../../components/atoms/typography'
 
 export const AboutMe = () => (
-  <PageShell withMainNavigation={true} title="About Me"><div>Something AboutMe</div></PageShell>
+  <PageShell withMainNavigation={true} title="About Me">
+    <HeaderTitle>
+      <H1>Ich bin Thomas Moser</H1>
+      <H2>Frontend Web Developer und Projektleiter</H2>
+      <P>React.js</P>
+    </HeaderTitle>
+    <img src="/assets/images/thomas_moser_3-1000w.jpg" width="400"/>
+  </PageShell>
 )
+const HeaderTitle = styled.div`
+  text-align: center;
+  margin-top: 7em;
+  margin-bottom: 12em;
+`
