a8173f82d99396223bb6353e86768c7514ad1175
Thomas Moser <mail@thomasmoser.ch>
2017-12-10 14:54:19 +0100

    feat: Added skeleton and todos to store data in indexedDB in Cache

--- src/components/data/cv-entries.js
+++ src/components/data/cv-entries.js
@@ -11,6 +11,8 @@ const CVEntriesState = ( { data : { loading=false, allCVEntrieses=false }} ) => 
   if(loading){
     return(<CVIsLoading />)
   } else if (allCVEntrieses){
+    //TODO: Put data in Cache with name tm-cv-entries
+    console.log(allCVEntrieses)
     return(<CV allCVEntrieses={allCVEntrieses}/>)
   } else {
     return(
@@ -21,7 +23,7 @@ const CVEntriesState = ( { data : { loading=false, allCVEntrieses=false }} ) => 
 }
 
 /*
-  here we create a grapphql operation
+  here we create a graphql operation
   there is an id needed for each graphql object
  */
 
@@ -57,7 +59,29 @@ const data = gql`
   We then can use the graphql container to pass the query results returned by getEntriesQuery
   to a component as a prop (and update them as the results change)
 */
-export const CVEntriesWithData = graphql(data)(CVEntriesState);
+
+const CVEntriesWithData = (...props) => {
+  const dataInCache = checkIfDataInCache()
+  if(dataInCache){
+    return <CVEntriesWithDataFromCache {...dataInCache}/>
+  }else{
+    return <CVEntriesWithDataFromAPI {...props}/>
+  }
+}
+
+const checkIfDataInCache = () => {
+  //TODO: Check if IndexedDB with the name tm-cv-entries exists, return true if yes
+  return false
+}
+
+const CVEntriesWithDataFromAPI = graphql(data)(CVEntriesState);
+
+const CVEntriesWithDataFromCache = () => {
+  let allCVEntriesesfromCache = {}
+  //TODO: Get data from indexedDB with the name tm-cv-entries
+  return <CV allCVEntrieses={allCVEntriesesfromCache}/>
+}
+
 
 //TODO: Replace this with a HOC
 
