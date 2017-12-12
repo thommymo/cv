71ecaddfbcd2264f7d6e7fefe2ee32b506f4d2ae
Thomas Moser <mail@thomasmoser.ch>
2017-12-12 19:23:04 +0100

    feat: Added print css for atom containers

--- src/components/atoms/containers.js
+++ src/components/atoms/containers.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
+import { print } from '../../utils/breakpoints'
 
 //TODO: Check if section is the correct html paragraph for this.
 
 export const Section = styled.div`
   padding-bottom: 5em;
+  ${print.paper`margin-bottom:0em;`}
 `
 export const SectionCentered = Section.extend`
   text-align: center;
@@ -12,4 +14,6 @@ export const HeaderTitle = styled.div`
   text-align: center;
   margin-top: 7em;
   margin-bottom: 9em;
+  ${print.paper`margin-top:4em;`}
+  ${print.paper`margin-bottom:4em;`}
 `
